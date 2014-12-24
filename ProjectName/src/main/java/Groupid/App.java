df48d67a5ecc38e3e2798e1094ccdecbc70b7cc5
jmason <jack.mason1@gmail.com>
2014-12-24 13:37:43 -0500

    adding projectName

---
+++ ProjectName/src/main/java/Groupid/App.java
@@ -0,0 +1,18 @@
+package Groupid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void hey()
+    {
+        System.out.println("hey");
+    }
+}
